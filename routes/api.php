ec3e170b035ccd75dfef9f462c8eaaf63ba91bb8
Andrey <aevsergeevich@gmail.com>
2024-02-11 15:25:49 +0400

    Добавлены маршруты авторизации

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login', [Api\AuthController::class, 'login'])
+    ->name('login');
+Route::post('/logout', [Api\AuthController::class, 'logout'])
+    ->name('logout');
+
