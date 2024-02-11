84c28777886f1a5ab84628f19a91d1caee8df317
Andrey <aevsergeevich@gmail.com>
2024-02-11 17:23:27 +0400

    Добавлен контроллер для разрешений

---
+++ app/Http/Controllers/Api/Admin/PermissionsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Permission;
+use Illuminate\Http\Request;
+use App\Http\Requests\Permissions;
+
+class PermissionsController extends Controller
+{
+    public function index()
+    {
+        $permissions = \App\Jobs\Permissions\Index::dispatchSync();
+
+        return response()->json(['permissions' => $permissions], 200);
+    }
+
+    public function show($permissionsId)
+    {
+        $permissions = \App\Jobs\Permissions\Show::dispatchSync($permissionsId);
+
+        return response()->json(['permissions' => $permissions], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $permissions = \App\Jobs\Permissions\Create::dispatchSync(
+            project_id: $request->project_id,
+            user_id: $request->user_id,
+            fields: $request->fields,
+            manage_leads: $request->manage_leads,
+            export_data: $request->export_data,
+            manage_permissions: $request->manage_permissions,
+            manage_settings: $request->manage_settings,
+            manage_project: $request->manage_project,
+        );
+
+        return response()->json(['success' => 'Разрешение успешно добавлено'], 200);
+    }
+
+    public function update(Request $request, $permissionsId)
+    {
+        $permissions = \App\Jobs\Permissions\Update::dispatchSync(
+            permissionsId: $permissionsId,
+            project_id: $request->project_id,
+            user_id: $request->user_id,
+            fields: $request->fields,
+            manage_leads: $request->manage_leads,
+            export_data: $request->export_data,
+            manage_permissions: $request->manage_permissions,
+            manage_settings: $request->manage_settings,
+            manage_project: $request->manage_project,
+        );
+
+        return response()->json(['success' => 'Разрешение успешно обновлено'], 200);
+    }
+
+    public function destroy(Request $request, $permissionsId)
+    {
+        $permissions = \App\Jobs\Permissions\Delete::dispatchSync($permissionsId);
+
+        return response()->json(['success' => 'Разрешение успешно удалено'], 200);
+    }
+}
